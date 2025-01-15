53db95a2f62b409ef257513c4af90f37c40be685
lbwnb <577280083@qq.com>
2025-01-15 20:47:48 +0800

    update

--- Music in HTML/js/music.js
+++ Music in HTML/js/music.js
@@ -206,6 +206,41 @@ music.onended = () => {
             break
     }
 }
+//实现移动按钮改变音乐播放位置
+player_progress_dot.onmousedown = (event) => {
+    cancelAnimationFrame(requestID)
+    let initX = event.clientX
+    let startX = player_progress_played.offsetWidth
+    document.onmousemove = (event) => {
+        let moveX = event.clientX
+        let newX = moveX - initX + startX
+        if (0 < newX && newX < player_progress_inner.offsetWidth) {
+            player_progress_dot.style.left = newX + "px"
+            player_progress_played.style.width = newX + "px"
+            music.currentTime = (newX / player_progress_inner.clientWidth) * music_time
+        }
+    }
 
+    document.onmouseup = () => {
+        document.onmousemove = null
+        update()
+    }
+}
+//实现移动按钮改变音量
+voice_dot.onmousedown = (event) => {
+    let initX = event.clientX
+    let startX = voice_overed.offsetWidth
+    document.onmousemove = (event) => {
+        let moveX = event.clientX
+        let newX = moveX - initX + startX
+        if (0 < newX && newX < voice_inner.offsetWidth) {
+            voice_dot.style.left = newX + "px"
+            voice_overed.style.width = newX + "px"
+            music.volume = newX / voice_inner.clientWidth
+        }
+    }
 
-
+    document.onmouseup = () => {
+        document.onmousemove = null
+    }
+}
